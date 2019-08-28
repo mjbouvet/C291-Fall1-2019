2846eba6792e0e006865b6a92008581c75a7bbd0
Michael John Bouvette <mjbouvet@silo.soic.indiana.edu>
2019-08-28 17:23:11 -0400

    assignment 0

--- exercises/wk1/hello.c
+++ exercises/wk1/hello.c
@@ -2,17 +2,6 @@
 #include <unistd.h>
 
 int main(void) {
-	printf(" \"{Hello! Welcome to a Beautiful Fall @ IU!},/\\ This is Fall 2019's 1st 8 Week Course\"\n");	
-	printf("\"\"\\\\/\n");
-	int i;
-	while(1){
-       	 printf("Michael Bouvette");
-          fflush(stdout);
-		 for(i=1; i<=5;i++){
-               	 printf(".");
-               	 sleep(1);
-		 fflush(stdout);}
-	printf("\n");
-	sleep(30);}
-  return 0;
+        printf("Michael Bouvette\n");
+	return 0;
 }
