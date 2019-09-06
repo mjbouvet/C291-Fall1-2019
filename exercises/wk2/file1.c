e3233cefcf0398c800d08def209750dc677cd067
Michael John Bouvette <mjbouvet@silo.soic.indiana.edu>
2019-09-06 07:47:14 -0400

    debugged file1

--- exercises/wk2/file1.c
+++ exercises/wk2/file1.c
@@ -9,14 +9,18 @@
 
 int main (void){
 
-  char limit = 1000, sum = 0, counter;
-  double alphabet = 'A';
-  printf("The limit is %f\n", &limit);
-  for(counter = 2; counter < limit; counter++){
-    sum = counter;
-    
-
-      printf("%f\n",alphabet++);  
+  int limit = 100, sum = 0, counter;
+  char  alphabet = 'A';
+  printf("The limit is %d\n", limit);
+  for(counter = 2; counter <= limit; counter++){
+    if(counter % 2 == 0)
+	sum += counter;
+    if(counter < 28){
+      printf("%c\n",alphabet++);}  
   }
-    printf("Sum = %d\n" limit);
+    printf("Sum = %d\n", sum);
 }
+
+//Wasn't sure if the 100 was inclusive so to prove that I know what is going on
+//If the 100 was not inclusive then the sum would be 2450 in the first if statement because
+//the 100 would not be added into the total
