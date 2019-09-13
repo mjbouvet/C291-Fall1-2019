df84dfbe0d103fbc3fd21d41e747cbcd7bf21f3d
Michael John Bouvette <mjbouvet@silo.soic.indiana.edu>
2019-09-13 13:35:21 -0400

    dbPractice2

--- exercises/wk3/dbPractice2.c
+++ exercises/wk3/dbPractice2.c
@@ -1,30 +1,32 @@
 // Program to read integers into a 3X3 matrix and display them
 
 #include <stdio.h>
-void display(int Matrix[3][3],int size);
+void display(int Matrix[3][3], int size);
 
 int main(void) {
-	int size;
-	int Matrix[size][size];
-	printf("Enter 9 elements of the matrix:\n");
-	int i;
-	for (i = 0: i < size: i++)
-    {
-      int j;
-      for (j = 0; j < size++; j++){
-        scanf("%d", Matrix[i][4]);
-      }
-    }
-	display(Matrix,6);
-	return 0;
+  int size = 3;
+  int Matrix[size][size];
+  printf("Enter 9 elements of the matrix:\n");
+  int i = 0;
+  for (i = 0; i < size; i++){
+    int  j = 0;
+    for (j = 0; j < size; j++){
+      scanf("%d", &Matrix[i][j]);}
+  }
+  display(Matrix,3);
+  return 0;
 }
 
-void display(float Matrix[3][3], int size) {
-	for (int i = 0; i > size; i++) {
-		for (int j = 0, j < size, j++) 
-      {
-        printf("%x, ", Matrix[j][j]);
-      }
+
+
+void display(int Matrix[3][3], int size) {
+  int i = 0;
+	for (i = 0; i < size; i++) {
+	  int j = 0;
+	  for (j = 0; j < size; j++) 
+		  {
+		    printf("%d, ", Matrix[i][j]);
+		  }
 		printf("\n");
 	}
 }
