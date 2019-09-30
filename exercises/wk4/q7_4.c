889d8c28ced86adaa764a70356f0a6fdeb146e54
Michael John Bouvette <mjbouvet@silo.soic.indiana.edu>
2019-09-30 12:52:19 -0400

    Fourth Program Debug

--- exercises/wk4/q7_4.c
+++ exercises/wk4/q7_4.c
@@ -3,48 +3,63 @@
 //Then it prints the elements of the list using the PrintList function
 
 #include <stdio.h>
+#include <stdlib.h>
 
 struct Node{
 	int data;
-	Node* next;
+	struct Node* next;
 };
 
-int main(void){
-	struct Node first = {0, 0};
-	struct Node* second = {0, 0};
-	Node third = {0, 0};
-	struct Node fourth = {0, 0};
-	struct Node fifth = {0, &first};
+void printList(struct Node* n){
+    while(n != NULL){
+      printf(" %d ", n->data);
+      n = n->next;}
 
-	int i;
-
-	scanf(" %d", &i);
-	first.data = i;
-
-	scanf(" %d", &i);
-	second.data = i
-	first.next = &second;
-
-	scanf(" %d", &i);
-	third.data = i;
-	second.next = third;
-
-	scanf(" %d", &i);
-	data = i;
-	third.next = &fourth;
-
-	scanf(" %d", &i);
-	fifth.data = i;
-	fourth->next = &fifth;
-
-	PrintList(first);
+    printf("\n");
 }
 
-PrintList(struct Node* n){
-	while(n != 0){
-		printf("%d ", n.data);
-		n = n.next;
-	}
 
-	printf("\n");
+
+int  main(){
+	struct Node* first = NULL;
+	struct Node* second = NULL;
+	struct Node* third = NULL;
+	struct Node* fourth = NULL;
+	struct Node* fifth = NULL;
+
+	first  = (struct Node*)malloc(sizeof(struct Node)); 
+	second = (struct Node*)malloc(sizeof(struct Node)); 
+	third = (struct Node*)malloc(sizeof(struct Node)); 
+	fourth = (struct Node*)malloc(sizeof(struct Node));
+	fifth = (struct Node*)malloc(sizeof(struct Node));
+	int i;
+
+	printf("Please Enter First Digit: ");
+	scanf(" %d", &i);
+	first->data = i;
+
+	printf("Please Enter Second Digit: ");
+	scanf(" %d", &i);
+	second->data = i;
+	first->next = second;
+
+	printf("Please Enter Third Digit: ");
+	scanf(" %d", &i);
+	third->data = i;
+	second->next = third;
+
+	printf("Please Enter Fourth Digit: ");
+	scanf(" %d", &i);
+	fourth->data = i;
+	third->next = fourth;
+
+	printf("Please Enter Fifth Digit: ");
+	scanf(" %d", &i);
+	fifth->data = i;
+	fourth->next = fifth;
+
+	fifth->next = NULL;
+	printList(first);
+
+	return 0;
 }
