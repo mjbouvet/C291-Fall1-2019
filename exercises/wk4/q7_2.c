f49c9ef280c9c26d4ea1a1bbdae6a4a7c9d99d0d
Michael John Bouvette <mjbouvet@silo.soic.indiana.edu>
2019-09-30 12:51:05 -0400

    Second Program Debug

--- exercises/wk4/q7_2.c
+++ exercises/wk4/q7_2.c
@@ -7,12 +7,12 @@
 #include <string.h>
 
 struct cat{
-	char name;
+	char name[20];
 	int age;
 	float weight;
 }
 
-int main(void){
+main(void){
 	char n[20];
 	int a;
 	float w;
@@ -20,16 +20,26 @@ int main(void){
 	printf("What is your cat's name: ");
 	scanf(" %s", n);
 
-	printf("How old is your cat:" );
+	printf("How old is your cat: " );
 	scanf(" %d", &a);
 
 	printf("How much does your cat weigh: ");
-	scanf(" %f", %w);
+	scanf(" %f", &w);
 
 	struct cat c;
-	struct cat *cPtr = c;
+	char ch = n[0];
+	int i = 0;
+	while(ch != '\0'){
+	  c.name[i] = ch;
+	  i++;
+	  ch = n[i];}
+	c.age = a;
+	c.weight = w;
+	struct cat *cPtr = &c;
 
-	printf("Cat's name is %s, and should be the same as %s.\n", c.name, cPtr);
-	printf("Cat's age is %d, and should be the same as %p.\n", c(age), cPtr);
-	printf("Cat's weight is %f, and should be the same as %f.\n", c,weight, cPtr->weight);
+	
+
+	printf("Cat's name is %s, and should be the same as %s.\n", c.name, cPtr->name);
+	printf("Cat's age is %d, and should be the same as %d.\n", c.age, cPtr->age);
+	printf("Cat's weight is %f, and should be the same as %f.\n", c.weight, cPtr->weight);
 }
