415c943810b9c4f78db74a4abad537b2301982a0
Michael John Bouvette <mjbouvet@silo.soic.indiana.edu>
2019-09-30 12:50:30 -0400

    First Program Debug

--- exercises/wk4/q7_1.c
+++ exercises/wk4/q7_1.c
@@ -5,16 +5,20 @@
 #include <string.h>
 
 int main(void){
-	char s[];
-	scanf(" %c", s);
+  char s[100];
+  printf("Please Input a string: ");
+	scanf(" %s", s);
 
-	char *cPtr = s[1];
+	char *cPtr = s;
+	char ch = s[0];
 
+	int size = sizeof(s)/sizeof(s[0]);
 	int i=0;
-	while(1){
-		printf("%c", cPtr+i);
-		i++;
-	}
-
+	while(ch != '\0'){
+	  ch = *(cPtr + i);
+	  printf("%c", ch);
+	  i++;
+	  
+	}	
 	printf("\n");
 }
